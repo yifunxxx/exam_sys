fc562c5c39b94a5d069c5e872cbead4891d9ebb4
yifun <xsaf1207@163.com>
2020-12-23 15:16:01 +0800

    新建用户接口

--- src/main/java/cn/xsaf1207/sys/controller/UserController.java
+++ src/main/java/cn/xsaf1207/sys/controller/UserController.java
@@ -4,29 +4,31 @@ import cn.xsaf1207.sys.service.UserService;
 import cn.xsaf1207.utils.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Repository
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/alluser")
+    @GetMapping("/alluser")
     public ResultData alluser(){
         return null;
     }
 
-    @RequestMapping("/updateuser")
+    @RequestMapping(value = "/updateuser",method = RequestMethod.PUT)
     public ResultData updateuser(){
         return null;
     }
 
-    @RequestMapping("/resetpas")
+    @RequestMapping(value = "/resetpas",method = RequestMethod.PUT)
     public ResultData resetpas(){
         return null;
     }
 
-    @RequestMapping("/deleteuser")
+    @RequestMapping(value = "/deleteuser",method = RequestMethod.DELETE)
     public ResultData deleteuser(){
         return null;
     }
